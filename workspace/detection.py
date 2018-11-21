7e3997e8cbfe86ae50542c23b267cc9bd55fc89d
Dmitry <zaonmax@inbox.ru>
2018-11-21 16:41:49 +0800

    log

--- workspace/detection.py
+++ workspace/detection.py
@@ -16,6 +16,10 @@ dataset_val = ShapesDataset()
 dataset_val.load_shapes(50, inference_config.IMAGE_SHAPE[0], inference_config.IMAGE_SHAPE[1])
 dataset_val.prepare()
 
+dataset_train = ShapesDataset()
+dataset_train.load_shapes(500, config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1])
+dataset_train.prepare()
+
 # Recreate the model in inference mode
 model = modellib.MaskRCNN(mode="inference", 
                           config=inference_config,
